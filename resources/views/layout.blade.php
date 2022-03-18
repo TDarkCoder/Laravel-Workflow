600125901782c2b3b3286de527bcd442a0853f39
Azizbek Khalilov <56731393+TDarkCoder@users.noreply.github.com>
2022-03-19 00:23:28 +0500

    Fetch third party css and js files locally

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,10 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') ?? 'Laravel workflow' }}</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://unpkg.com/bpmn-js@9.0.3/dist/assets/bpmn-js.css">
+    <link href="{{ asset('css/fonts.css') }}" rel="stylesheet">
+    <link
+        href="{{ asset('css/bootstrap.min.css') }}"
+        rel="stylesheet"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/bpmn-js.css') }}">
     <style>
         body {
             font-family: 'Nunito', sans-serif;
@@ -17,7 +19,6 @@
             min-height: calc(100vh - 128px);
         }
     </style>
-    <link rel="stylesheet" href="{{ public_path('app.css') }}">
 </head>
 <body>
 <x-header/>
@@ -33,12 +34,9 @@
 
 <x-footer/>
 
-<script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous">
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}" crossorigin="anonymous">
 </script>
-<script src="https://unpkg.com/bpmn-js@9.0.3/dist/bpmn-navigated-viewer.development.js"></script>
+<script src="{{ asset('js/bpmn-navigated-viewer.js') }}"></script>
 <script defer>
     const canvasContainer = document.getElementById('canvas-container');
     const canvas = document.getElementById('canvas');
